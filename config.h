dd10a069bda7073e990d86ea3ac5dc0c972710d4
th3lusive <brandond44@gmail.com>
2019-03-14 21:00:24 +0800

    Adjustments

--- config.h
+++ config.h
@@ -2,8 +2,5 @@
 
 static const Pair pairs[] = {
 	/* regex                  action */
-	{ "\.(jpg|png|tiff)$",    "sxiv.sh %s 2>/dev/null"        },
 	{ "\.gif$",               "mpv --loop %s 2>/dev/null" },
-	{ "\.mp3$",               "termite -e mpv %s" },
-	{ "^(http://|https://)?(www\.)?(youtube.com/watch\?|youtu\.be/)", "mpv %s 2>/dev/null" }
 };
